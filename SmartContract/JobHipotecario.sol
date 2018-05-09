4ae447ae96226e4c4ec7a2e7943fa4bdc0dd241b
Jdornemann <johann.dornemann.c@gmail.com>
2018-05-09 06:10:25 -0300

    v3 avance
    v3 avance

--- SmartContract/JobHipotecario.sol
+++ SmartContract/JobHipotecario.sol
@@ -12,38 +12,38 @@ namespace Neo.SmartContract
         public static readonly byte[] Owner4 = "ssfdfghhJHggfffdDDFHJJJHGFg".ToScriptHash(); // CBRS
         public static readonly byte[] Owner5 = "2k2h2gksjhshgsgsgsggfffdfsss".ToScriptHash(); // INMOBILIARIA - FINALIZA SOLICITUD - FIN DEL PROCESO
         
+
+        [DisplayName("transfer")]
+        public static event Action<byte[], byte[], object[]> Transferred;
+
         public static object Main(string operation, params object[] args)
         {
           
          if (Runtime.Trigger == TriggerType.Application)
            {
-                 if (operation == "ConsultarEstadoCredito") //Usuario consulta el estado de su credito
+                 if (operation == "ConsultarprocesoCredito") //Usuario consulta el proceso de su credito
                   {
                     if (args.Length != 1) return 0;
-                    String[] EstadoHipo = (byte[])args[0];
-                    return Consulta(EstadoHipo);
+                    String[] procesoHipo = (byte[])args[0];
+                    return Consulta(procesoHipo);
                   }
 
                  if (operation == "IngresarHipoInicial")  //Inicia la solicitud de Hipotecario (Banco)
                   {
                     if (args.Length != 4) return false;
                     byte[] Owner = (byte[])args[0];    // Entidad 1
-                    int idOperacion = (int)args[1];    
-                    string estado = (string)args[2];
-                    string fechaaccion = (string)args[3];
-                    return IngresarHipoInicial(Owner, idOperacion, estado,fechaaccion); // 
+                  
+                    return IngresarHipoInicial(Owner, args); // 
                   }
              
-                if (operation == "TraspasarSolProceso")  //Inicia la solicitud de Hipotecario (Banco) Cambios de estado
+                if (operation == "TraspasarSolProceso")  //Inicia la solicitud de Hipotecario (Banco) Cambios de proceso
                   {
                     if (args.Length != 5) return false;
                     byte[] from = (byte[])args[0];    // Entidad 1
                     byte[] to = (byte[])args[1];      // Entidad 2
-                    int IdOperacion = (int)args[2]; // Id de operacion 
-                    string estado = (string)args[3];
-                    string fechaaccion = (string)args[4];
+                   
 
-                    return Transfiere((from, to, IdOperacion,estado,fechaaccion); //Traspaso el trabajo a otra entidad (Siguiente en el proceso hipotecario)
+                    return Transfiere((from, to, args); //Traspaso el trabajo a otra entidad (Siguiente en el proceso hipotecario)
                   }
                 if (operation == "EliminarSolProceso") 
                     byte[] Owner = Owner1
@@ -53,19 +53,27 @@ namespace Neo.SmartContract
            }
         }
 
-        private static byte[] Consulta(byte[] address) // Consulto estado de progreso (Entidad quien la tiene)
+        private static byte[] Consulta(byte[] address) // Consulto proceso de progreso (Entidad quien la tiene)
         {
              return Storage.Get(Storage.CurrentContext, address).AsInteger();
         }
 
-        private static bool IngresarHipoInicial(byte[] owner,int idOperacion, string estado, string fechaaccion)
+        private static bool IngresarHipoInicial(byte[] owner,object[] args)
         {
-            if (!Runtime.CheckWitness(owner)) return false;
+             if (args.Length != 3)
+                return false;
 
-                object[] DatosHipo = new object[4];
-                DatosHipo[0] = idOperacion;    
-                DatosHipo[1] = fechaAccion;        
-                DatosHipo[2] = estado;      
+             if (!Runtime.CheckWitness(owner)) return false;
+
+             int idOperacion = (int)args[1];    
+             string proceso = (string)args[2];
+             string fechaaccion = (string)args[3];
+
+    
+            object[] DatosHipo = new object[3];
+            DatosHipo[0] = idOperacion;    
+            DatosHipo[1] = fechaAccion;        
+            DatosHipo[2] = proceso;      
 
             byte[] DatosHipo_storage = DatosHipo.Serialize();
 
@@ -74,14 +82,46 @@ namespace Neo.SmartContract
             return true;
         }
 
-       private static bool Transfiere(byte[] from, byte[] to, BigInteger value)
+       private static bool Transfiere(byte[] from, byte[] to, object[] args)
         {
-            if (!Runtime.CheckWitness(to)) return false;
-            byte[] from = Storage.Get(Storage.CurrentContext, dominio);
-            if (from == null) return false;
+            int IdOperacion = (int)args[2]; // Id de operacion 
+            string proceso = (string)args[3];
+            string fechaaccion = (string)args[4];
+
+
+           // if (proceso != 3) return false;
             if (!Runtime.CheckWitness(from)) return false;
-            Storage.Put(Storage.CurrentContext, dominio, to);
+            if (from == to) return true;
+
+
+             byte[] from_datos = Storage.Get(Storage.CurrentContext, from);
+
+              object[] datos = (object[])from_datos.Deserialize();
+
+            if (datos[0] != null) && (datos[1] == 3) // si idoperacion no es nulo y proceso = 3
+            {
+                  args[2] = IdOperacion
+                  args[3] = 0  // Inicia con estado 0
+                  args[4] = date.now 
+
+                byte[] datos_storage = args.Serialize();
+                Storage.Put(Storage.CurrentContext, from, datos_storage);
+
+                byte[] to_datos = Storage.Get(Storage.CurrentContext, to);
+
+                Storage.Put(Storage.CurrentContext, to, to_datos);
+                
+                Transferred(from, to, value);
             return true;
+            }
+
+            // bool authorized = false;
+             //if (!authorized)
+            //{
+
+           // }
+
+           // return true;
         }
 
        private static bool EliminarSolHipo(byte[] Owner)
